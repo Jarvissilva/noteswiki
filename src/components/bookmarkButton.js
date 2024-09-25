0ec6733443a0c6c4f0db6143491d922d649bc7d9
Jarvis Silva <77526704+Jarvissilva@users.noreply.github.com>
2024-09-25 13:24:29 +0530

    Update bookmarkButton.js

--- src/components/bookmarkButton.js
+++ src/components/bookmarkButton.js
@@ -1,43 +1,42 @@
-"use client"; // Ensure client-side rendering
-
+"use client"; 
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { MdBookmarkBorder, MdBookmark } from "react-icons/md";
 
 export default function BookmarkButton() {
-  const pathname = usePathname();
-  const [bookmarks, setBookmarks] = useState([]);
+  const pathname= usePathname()
+  const [bookmarks,setBookmarks] = useState([])
+  const [isBookmarked,setIsBookmarked] = useState(false)
 
-  // Load bookmarks from localStorage on component mount
-  useEffect(() => {
-    const storedBookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
-    setBookmarks(storedBookmarks);
-  }, []); // Empty dependency array ensures this runs only once on mount
-
-  // Save bookmarks to localStorage whenever they change
-  useEffect(() => {
-    if (bookmarks.length >= 0) {
-      // Ensure that even empty arrays are saved
-      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  useEffect(() =>{
+    const foundBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+    setBookmarks(foundBookmarks)
+    if(foundBookmarks && foundBookmarks.includes(pathname)){
+      setIsBookmarked(true)
     }
-  }, [bookmarks]);
-
-  const handleClick = () => {
-    const existBookmark = bookmarks.includes(pathname);
-
-    if (!existBookmark) {
-      setBookmarks((prev) => [...prev, pathname]); // Use functional update to ensure state consistency
-    } else {
-      const updatedBookmarks = bookmarks.filter((slug) => slug !== pathname);
-      setBookmarks(updatedBookmarks); // Update bookmarks with the filtered array
+    else{
+      setIsBookmarked(false)
     }
-  };
+  },[])
 
-  const isBookmarked = bookmarks.includes(pathname);
+  const handleClick = () =>{
+    if(!isBookmarked){
+      const newBookmarks = [...bookmarks, pathname]
+      localStorage.setItem('bookmarks',JSON.stringify(newBookmarks))
+      setBookmarks(newBookmarks);
+      setIsBookmarked(true)
+    }
+    else{
+      const updatedBookmarks = bookmarks.filter(bookmark => bookmark!==pathname)
+      localStorage.setItem('bookmarks',JSON.stringify(updatedBookmarks))
+      setBookmarks(updatedBookmarks)
+      setIsBookmarked(false)  
+    }
+  }
 
   return (
     <button onClick={handleClick}>
-      {isBookmarked ? <MdBookmark size={40} /> : <MdBookmarkBorder size={40} />}
+      {isBookmarked ? <MdBookmark size={40}/> : <MdBookmarkBorder size={40}/>}
     </button>
   );
 }
